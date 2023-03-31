7ea2486ec9ebbbb6116c3044727daf6cea42744b
Mateus Hilario Dias <118845101+mateushilariodiasnh@users.noreply.github.com>
2023-03-31 16:31:11 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,54 @@
-const enviar = document.getElementById('#enviar')
-
-// resultado = media >= 6 ? "Aprovado" : "Reprovado"
-
 class CadastroDeAlunos {
     constructor() {
         this.alunosCadastrados = []
-        // this.nome
-        // this.ra
-        // this.idade
-        // this.sexo
-        // this.media
-        // this.resultado
     }
     salvarAluno() {
         let aluno = this.lerDados()
-
-        console.log(aluno)
+        if(this.validarCampos(aluno)){
+            this.adicionarAluno(aluno)
+        }
+        console.log(this.alunosCadastrados)
+        var cadastroJSON = JSON.stringify(cadastroDeAlunos)
+        var dadosCodificados = encodeURIComponent(cadastroJSON)
+/*         window.location.href='menu-principal.html' + dadosCodificados */
+        console.log(cadastroJSON)
+    }
+s
+    adicionarAluno(aluno) {
+        this.alunosCadastrados.push(aluno)
     }
 
     lerDados() {
         let aluno = {}
-
-        aluno.nome  = document.querySelector('#nome').value
-        aluno.ra = document.querySelector('#ra').value
-        aluno.idade = document.querySelector('#idade').value
-        aluno.sexo = document.querySelector('#sexo').value
-        aluno.media = document.querySelector('#media').value
-        aluno.resultado = document.querySelector('#resultado').value
-        
+        aluno.nome  = document.getElementById('nome').value;
+        aluno.ra = document.getElementById('ra').value;
+        aluno.idade = document.getElementById('idade').value;
+        aluno.sexo = document.getElementById('sexo').value;
+        aluno.media = document.getElementById('media').value;
+        aluno.resultado = document.getElementById('resultado').value;
         return aluno
+    }
+
+    validarCampos(aluno) {
+        let mensagemDeAlerta = ''
+
+        if(aluno.nome == ''){
+            mensagemDeAlerta += '- informe o seu nome \n'
+        }
+        if(aluno.ra == ''){
+            mensagemDeAlerta += '- informe o seu RA \n'
+        }
+        if(aluno.idade == ''){
+            mensagemDeAlerta += '- informe a sua idade \n'
+        }
+        if(aluno.media == ''){
+            mensagemDeAlerta += '- informe a sua média \n'
+        }
+        if(mensagemDeAlerta != ''){
+            alert(mensagemDeAlerta)
+            return false
+        }
+        return true
     }
 
     cadastrarAluno() {
